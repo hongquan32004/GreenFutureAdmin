b71339cd4428d6cb52a6516451f0112dd306926c
Trần Hồng Quân <117972211+hongquan32004@users.noreply.github.com>
2025-04-04 23:19:49 +0700

    feat: add login (#1)

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Login from "./page/login";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <Login />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
